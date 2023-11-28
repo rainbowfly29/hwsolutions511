1eee4a34c583bdd60478c331b0a84a9a749ca1ea
rainbowfly29 <147787823+rainbowfly29@users.noreply.github.com>
2023-11-27 23:44:08 -0500

    final changes

--- hw11.lean
+++ hw11.lean
@@ -22,25 +22,59 @@ open Function
 
 /- 2 points -/
 theorem problem4a : ¬ ∀ (f : ℤ → ℤ), Surjective f → Surjective (fun x ↦ 2 * f x) := by
-  sorry
+  dsimp [Surjective]
+  push_neg
+  use fun x ↦ x
+  constructor
+  · intro a
+    use a
+    ring
+  · use 1
+    intro x
+    simp
+    obtain hzero | hone := le_or_succ_le x 0
+    · apply ne_of_lt --x <= 0
+      calc
+        2 * x ≤ 2 * 0 := by rel[hzero]
+        _ = 0 := by ring
+        _ < 1 := by extra
+    · apply ne_of_gt --x >= 1
+      calc
+        2 * x ≥ 2 * 1 := by rel[hone]
+        _ = 1 + 1 := by ring
+        _ > 1 := by extra
 
 /- 2 points -/
 theorem problem4b : ¬ ∀ c : ℝ, Surjective (fun x ↦ c * x) := by
   dsimp[Surjective]
   push_neg
-  use 0, 1 
+  use 0, 1
   intro x
   ring
   numbers
 
 /- 3 points -/
 theorem problem4c {f : ℚ → ℚ} (hf : ∀ x y, x < y → f x < f y) : Injective f := by
-  sorry
+  dsimp [Injective]
+  intro a b h
+  obtain hl | hab | hg := lt_trichotomy a b
+  . have hx := ne_of_lt (hf a b hl)
+    contradiction
+  . apply hab
+  . have hx := ne_of_gt (hf b a hg)
+    contradiction
 
 /- 3 points -/
 theorem problem4d {f : X → ℕ} {x0 : X} (h0 : f x0 = 0) {i : X → X}
     (hi : ∀ x, f (i x) = f x + 1) : Surjective f := by
-  sorry
+  dsimp[Surjective]
+  intro a
+  simple_induction a with x hx
+  . use x0
+    apply h0
+  . obtain ⟨a, hx⟩ := hx
+    use i a
+    rw [hi, hx]
 
 /- 2 points -/
 theorem problem5a : Bijective (fun (x : ℝ) ↦ 4 - 3 * x) := by
@@ -98,6 +132,6 @@ theorem problem5c : Inverse u v := by
 theorem problem5d {f : X → Y} (hf : Injective f) {g : Y → Z} (hg : Injective g) :
     Injective (g ∘ f) := by
   dsimp[Injective] at *
-  intro a b 
+  intro a b
   intro hab
   apply hf (hg hab)
